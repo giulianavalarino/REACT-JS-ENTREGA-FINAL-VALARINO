b84c07d11a9cfb8bf7bed9f601f64c2dc65811e8
giuliana valarino <gvalarino2015@gmail.com>
2023-01-16 12:12:41 -0300

    Creacion de la sidebar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,25 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/button/button';
+import Sidebar from './components/sidebar/sidebar';
 
 function App() {
+  const [isOpen, setOpen] = useState(false);
+  const onHandlerClick = () => {
+    setOpen(!isOpen);
+  }
   return (
     <div className="App">
+      <Sidebar onClose={onHandlerClick} isOpen={isOpen}>
+        <h2>Item</h2>
+      </Sidebar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button text='Click me' onHandlerClick={onHandlerClick}/>
         <a
           className="App-link"
           href="https://reactjs.org"
